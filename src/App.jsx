93a46ced4720adfb76c2774ea48dae2113d72359
KrishnaOnline <krishnavamshikusuma@gmail.com>
2023-09-06 23:35:52 +0530

    02

--- src/App.jsx
+++ src/App.jsx
@@ -5,59 +5,104 @@ import './App.css'
 
 function App() {
 
-  const [formData, setFormData] = useState({fName:"", lName:"", email:"", country:"", });
+  const [formData, setFormData] = useState({fName:"", mail:"", country:"India", chkbx1:false, chkbx2:false, chkbx3:false, radios:""})
+  console.log(formData)
 
-  const formHandler = (e) => {
+  function formHandler(e) {
+    // e.preventDefault();
     const {name, type, value, checked} = e.target;
     setFormData(prevData => {
       return {
         ...prevData,
-        [name]: type === "checkbox" ? checked : value
+        [name]: type === 'checkbox' ? checked : value
       }
     })
   }
 
   return (
-    <div className='flex flex-col items-center'>
-      <h1 className='m-5 mb-9 font-bold text-2xl'>React Froms Practice</h1>
-      <form className='flex flex-col gap-5'>
-        <input 
-          className='border border-black' 
-          placeholder='first name' 
-          type='text' 
+    <div>
+      <form className='flex flex-col items-center gap-3' onSubmit={formHandler}>
+        <h1 className='m-5 mb-9 font-bold text-2xl'>React Froms Practice</h1>
+        <label htmlFor='fName'>First Name</label>
+        <input
+          type='text'
+          id='fName'
           name='fName'
+          placeholder='fName'
+          onChange={formHandler}
+          value={formData.fName}
         />
 
+        <label htmlFor='mail'>Email</label>
         <input
-         className='border border-black' 
-         placeholder='last name' 
-         type='text' 
-         name='lName'
+          type='email'
+          id='mail'
+          name='mail'
+          placeholder='mail'
+          onChange={formHandler}
+          value={formData.mail}
         />
 
-        <input
-         className='border border-black' 
-         placeholder='your-mail@mail.com' 
-         type='email' 
-         name='email'
-        />
-
+        <label htmlFor='country'>Country</label>
         <select
-         className='border border-black' 
-         name='country' 
-         value={formData.country}
+          name='country'
+          id='country'
+          onChange={formHandler}
+          value={formData.country}  
         >
           <option value='India'>India</option>
-          <option value='United States of America'>United States of America</option>
-          <option value='China'>China</option>
-          <option value='Bangladesh'>Bangladesh</option>
-          <option value='Pakistan'>Pakistan</option>
+          <option value='USA'>USA</option>
+          <option value='Australia'>Australia</option>
+          <option value='Canada'>Canada</option>
         </select>
 
-        <label htmlFor='chkbx1'>CheckBox 1 <input type='checkbox' id='chkbx1'/></label>
-        <label htmlFor='chkbx1'>CheckBox 2 <input type='checkbox' id='chkbx1'/></label>
-        <label htmlFor='chkbx1'>CheckBox 3 <input type='checkbox' id='chkbx1'/></label>
-      </form>    
+        <label htmlFor='chkbx1'>CheckBox 1</label>
+        <input
+          type='checkbox'
+          name='chkbx1'
+          id='chkbx1'
+          checked={formData.chkbx1}
+          value={formData.chkbx1}
+          onChange={formHandler}
+        />
+        <label htmlFor='chkbx2'>CheckBox 2</label>
+        <input
+          type='checkbox'
+          name='chkbx2'
+          id='chkbx2'
+          checked={formData.chkbx2}
+          value={formData.chkbx2}
+          onChange={formHandler}
+        />
+        <label htmlFor='chkbx3'>CheckBox 3</label>
+        <input
+          type='checkbox'
+          name='chkbx3'
+          id='chkbx3'
+          checked={formData.chkbx3}
+          value={formData.chkbx3}
+          onChange={formHandler}
+        />
+
+        <label htmlFor='radios'>Radio Button 1</label>
+        <input
+          type='radio'
+          name='radios'
+          id='radios'
+          value='radio1'
+          onChange={formHandler}
+        />
+        <label htmlFor='radios'>Radio Button 2</label>
+        <input
+          type='radio'
+          name='radios'
+          id='radios'
+          value='radio2'
+          onChange={formHandler}
+        />
+
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
